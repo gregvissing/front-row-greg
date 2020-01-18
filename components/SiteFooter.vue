31719e33f49155a3ab2197fd21574c574c81f9d4
gregvissing <greg.vissing@gmail.com>
2020-01-18 14:41:47 -0500

    comment out text in footer

--- components/SiteFooter.vue
+++ components/SiteFooter.vue
@@ -2,23 +2,23 @@
   <footer class="site-footer">
     {{ $siteConfig.siteName }} &copy; {{ new Date().getFullYear() }} - All
     Rights Reserved
-    <br />
+    <!-- <br />
     Made with <span style="color: #e25555;">&#9829;</span> with Nuxt.js and
-    Netlify
+    Netlify-->
   </footer>
 </template>
 
 <script>
 export default {
-  name: 'SiteFooter'
+    name: 'SiteFooter'
 }
 </script>
 
 <style scoped>
 footer {
-  background: #eee;
-  padding: 20px 10px;
-  text-align: center;
-  transition: 0.5s ease all;
+    background: #eee;
+    padding: 20px 10px;
+    text-align: center;
+    transition: 0.5s ease all;
 }
 </style>
