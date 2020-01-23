a1244af945bcac819fe2bbbef52c752e155fe689
gregvissing <greg.vissing@gmail.com>
2020-01-23 12:36:42 -0500

    Update resources.vue

--- pages/resources.vue
+++ pages/resources.vue
@@ -11,41 +11,64 @@
         <div class="content">
           <h3>A11Y - Standards</h3>
           <p>
-            <ol>
-				<li><a href="https://www.w3.org/WAI/standards-guidelines/">W3C Accessibility Standards Overview</a></li>
-			</ol>
+            <ul>
+              <li><a href="https://www.w3.org/WAI/standards-guidelines/">W3C Accessibility Standards Overview</a></li>
+              <li><a href="https://www.w3.org/TR/WCAG20/">Web Content Accessibility Guidelines (WCAG) 2.0</a></li>
+            </ul>
           </p>
-		  <h3>A11Y - Tools</h3>
+          <h3>A11Y - How-To</h3>
           <p>
-            <ol>
-				<li><a href="https://marcozehe.de/2018/04/11/introducing-the-accessibility-inspector-in-the-firefox-developer-tools/">Accessibility Inspector (Firefox)</a></li>
-			</ol>
+            <ul>
+              <li><a href="https://www.creativebloq.com/advice/14-easy-ways-to-make-your-website-more-accessible">14 easy ways to make your website more accessible</a></li> 
+              <li><a href="https://labs.clio.com/a11y-at-clio-how-we-build-accessible-ui-web-components-b31d8e1fef23">A11y at Clio: How We Build Accessible UI Web Components</a></li>
+            </ul>
           </p>
+          <h3>A11Y - Strategy</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit!
+            <ul>
+              <li><a href="https://www.oho.com/blog/ada-website-compliance-preparing-your-college-or-university-website">ADA Website Compliance: Preparing Your College or University Website</a></li>
+              <li><a href="https://opensenselabs.com/blog/articles/checklist-comply-ada-accessibility-higher-education">Checklist to Comply with ADA Accessibility in Higher Education</a></li>
+              <li><a href="https://www.adaactionguide.org/action-steps">ADA Title II Action Guide for State and Local Governments</a></li>
+              <li><a href="https://medium.com/myplanet-musings/making-the-web-more-accessible-using-machine-learning-8a32eaafdb3a">Making the Web More Accessible Using Machine Learning</a></li>
+            </ul>
           </p>
-
+          <h3>A11Y - Policy Example</h3>
+          <p>
+            <ul>
+              <li><a href="https://usability.yale.edu/web-accessibility/accessibility-yale">Accessibility at Yale</a></li>
+            </ul>
+          </p>
+          <h3>A11Y - Tools</h3>
+          <p>
+            <ul>
+              <li><a href="https://marcozehe.de/2018/04/11/introducing-the-accessibility-inspector-in-the-firefox-developer-tools/">Accessibility Inspector (Firefox)</a></li>
+            </ul>
+          </p>
           <h3>A11Y - Forms</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit!
+            <ul>
+              <li><a href="https://marcozehe.de/2018/04/11/introducing-the-accessibility-inspector-in-the-firefox-developer-tools/">Accessibility Inspector (Firefox)</a></li>
+            </ul>
           </p>
-          <quote-card />
+          <!-- <quote-card /> -->
           <h3>A11Y - Testing</h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci
-            eligendi impedit inventore ipsa laboriosam libero magnam modi odio,
-            sunt temporibus. Distinctio doloremque eum magni nostrum quasi
-            ratione rem repudiandae sit! Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit.
-            <strong>Alias aut facere harum ipsam</strong> quibusdam quidem
-            quisquam saepe? Alias architecto blanditiis consequatur consequuntur
-            dicta ipsa iste modi provident, quod, similique veritatis.
+            <ul>
+              <li><a href="https://marcozehe.de/2018/04/11/introducing-the-accessibility-inspector-in-the-firefox-developer-tools/">Accessibility Inspector (Firefox)</a></li>
+            </ul>
+          </p>
+          <h3>A11Y - Legal</h3>
+          <p>
+            <ul>
+              <li><a href="https://www.d.umn.edu/~lcarlson/atteam/lawsuits.html">Higher Ed Accessibility Lawsuits, Complaints, and Settlements</a></li>
+              <li><a href="https://www.insidehighered.com/news/2018/11/06/universities-still-struggle-make-websites-accessible-all">Feds Prod Universities to Address Website Accessibility Complaints</a></li>
+            </ul>
+          </p>
+          <h3>A11Y - Influencers</h3>
+          <p>
+            <ul>
+              <li><a href="https://www.kpodnar.com/">Kristina Podnar</a></li>
+            </ul>
           </p>
         </div>
 
@@ -102,13 +125,13 @@
   </div>
 </template>
 <script>
-import QuoteCard from '~/components/cards/QuoteCard'
+// import QuoteCard from '~/components/cards/QuoteCard'
 export default {
   head() {
     return {
       title: `Resources | ${this.$siteConfig.siteName}`
     }
   },
-  components: { QuoteCard }
+//   components: { QuoteCard }
 }
 </script>
