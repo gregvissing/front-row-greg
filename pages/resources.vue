1ebed240b0d089bbb10628e72291c4d1ca1129b0
gregvissing <greg.vissing@gmail.com>
2020-01-23 13:42:25 -0500

    Update resources.vue

--- pages/resources.vue
+++ pages/resources.vue
@@ -14,13 +14,23 @@
             <ul>
               <li><a href="https://www.w3.org/WAI/standards-guidelines/">W3C Accessibility Standards Overview</a></li>
               <li><a href="https://www.w3.org/TR/WCAG20/">Web Content Accessibility Guidelines (WCAG) 2.0</a></li>
+              <li><a href="https://alistapart.com/article/standards-for-writing-accessibly/">Standards for Writing Accessibly</a></li>
+              <li><a href="https://developer.mozilla.org/en-US/docs/Learn/Accessibility/HTML">HTML: A good basis for accessibility</a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
             </ul>
           </p>
-          <h3>A11Y - How-To</h3>
+          <h3>A11Y - Navigation</h3>
           <p>
             <ul>
-              <li><a href="https://www.creativebloq.com/advice/14-easy-ways-to-make-your-website-more-accessible">14 easy ways to make your website more accessible</a></li> 
-              <li><a href="https://labs.clio.com/a11y-at-clio-how-we-build-accessible-ui-web-components-b31d8e1fef23">A11y at Clio: How We Build Accessible UI Web Components</a></li>
+              <li><a href="https://www.a11ymatters.com/pattern/mobile-nav/">Accessible Mobile Navigation</a></li>
+              <li><a href="https://medium.com/@linlinghao/accessibility-for-hamburger-menu-a37fa9617a89">Accessibility for Hamburger Menu</a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
             </ul>
           </p>
           <h3>A11Y - Strategy</h3>
@@ -30,31 +40,103 @@
               <li><a href="https://opensenselabs.com/blog/articles/checklist-comply-ada-accessibility-higher-education">Checklist to Comply with ADA Accessibility in Higher Education</a></li>
               <li><a href="https://www.adaactionguide.org/action-steps">ADA Title II Action Guide for State and Local Governments</a></li>
               <li><a href="https://medium.com/myplanet-musings/making-the-web-more-accessible-using-machine-learning-8a32eaafdb3a">Making the Web More Accessible Using Machine Learning</a></li>
+              <li><a href="https://www.creativebloq.com/advice/14-easy-ways-to-make-your-website-more-accessible">14 easy ways to make your website more accessible</a></li> 
+              <li><a href="https://labs.clio.com/a11y-at-clio-how-we-build-accessible-ui-web-components-b31d8e1fef23">A11y at Clio: How We Build Accessible UI Web Components</a></li>
+              <li><a href="https://designsystem.digital.gov/">United States Web Design System</a></li>
+              <li><a href="https://www.brucelawson.co.uk/2019/checklist-to-avoid-the-most-common-accessibility-errors/">Checklist to avoid the most common accessibility errors</a></li>
+              <li><a href="https://www.w3.org/WAI/tutorials/">Web Accessibility Tutorials</a></li>
+              <li><a href="http://simplyaccessible.com/article/keyboard-superhero/">How to be a keyboard accessibility super hero</a></li>
+              <li><a href="https://medium.com/@matuzo/writing-css-with-accessibility-in-mind-8514a0007939">Writing CSS with Accessibility in Mind</a></li>
+              <li><a href="https://www.cjcid.com/articles/unexpected-a11y-tips/">Unexpected accessibility tips</a></li>
+              <li><a href="https://webflow.com/blog/5-ways-content-can-improve-your-websites-accessibility-and-overall-ux">5 ways content can improve your websites’ accessibility — and overall UX</a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
             </ul>
           </p>
-          <h3>A11Y - Policy Example</h3>
+          <h3>A11Y - Misc Resources</h3>
+          <p>
+            <ul>
+              <li><a href="https://darekkay.com/blog/accessible-ui-frameworks/">The state of accessible web UI frameworks</a></li>
+              <li><a href="https://www.boye-co.com/blog/2019/10/3/accessibility-update-towards-2020">Accessibility Update Towards 2020</a></li>
+              <li><a href="https://css-tricks.com/growing-accessibility-conversations/">Growing Accessibility Conversations</a></li>
+              <li><a href="https://a11y.reviews/">Accessibility Reviews</a></li>
+              <li><a href="https://ishadeed.com/article/clickable-area/">Enhancing The Clickable Area Size</a></li>
+              <li><a href="https://www.24a11y.com/2019/web-components-and-the-aom/">Web Components and the Accessibility Object model (AOM)</a></li>
+              <li><a href="https://www.behance.net/gallery/12419409/VICE-VERSA-diagonal-UI-optimized-for-a-single-hand-IX">VICE VERSA - diagonal UI optimized for single hand IX</a></li>
+              <li><a href="https://marcysutton.github.io/js-a11y-workshop/">Accessibility in JavaScript Applications</a></li>
+              <li><a href="https://puxl.io/">PUXL - framework</a></li>
+              <li><a href="https://davatron5000.github.io/a11y-nutrition-cards/">Nutrition Cards for Accessible Components</a></li>
+              <li><a href="https://a11yproject.com/">The A11Y Project</a></li>
+              <li><a href="https://dylanb.github.io/periodic-aria-roles.html">Periodic Table of ARIA 1.0 Roles</a></li>
+              <li><a href="https://www.w3.org/TR/wai-aria-practices/">WAI-ARIA Authoring Practices 1.1</a></li>
+              <li><a href="https://www.w3.org/TR/wai-aria/">Accessible Rich Internet Applications (WAI-ARIA) 1.1</a></li>
+              <li><a href="https://developers.google.com/web/fundamentals/accessibility">Web Fundamentals - Accessibility</a></li>
+              <li><a href="https://www.telerik.com/blogs/accessibility-developers-user-story">Accessibility a Developer's User Story</a></li>
+              <li><a href="https://a11ysupport.io/">Accessibility Support</a></li>
+              <li><a href="https://developer.paciellogroup.com/blog/2013/12/using-aria-enhance-svg-accessibility/">Using ARIA to enhance SVG accessibility</a></li>
+              <li><a href="https://simplyaccessible.com/article/8-things-parenting-taught-accessibility/">8 things parenting taught me about accessibility</a></li>
+              <li><a href="https://www.sarasoueidan.com/blog/what-accessibility-taught-me/">Sara Soueidan - What a Year of Learning and Teaching Accessibility Taught Me</a></li>
+              <li><a href="https://www.deque.com/wcag/">Web Content Accessibility Guidelines (WCAG) Conformance</a></li>
+              <li><a href="https://strategybykatie.com/website-accessibility-how-you-can-make-your-website-function-for-people-of-all-abilities-even-if-youre-not-a-coder/">Website Accessibility – How you can make your website function for people of all abilities, even if you’re not a coder</a></li>
+              <li><a href="https://a11yproject.com/patterns/">The A11Y Project - Patterns</a></li>
+              <li><a href="https://ericwbailey.design/writing/truths-about-digital-accessibility.html">Truths about digital accessibility</a></li>
+              <li><a href="https://libguides.ctstatelibrary.org/dld/accessibility/websites">Libraries and Accessibility: Accessible Websites</a></li>
+              <li><a href="https://www.blackbaud.com/files/support/helpfiles/luminate-online/help/Subsystems/Administrator/Content/Ref/Admin_Accessibility_Checker.html">Using the Accessibility Checker on Web Pages</a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+            </ul>
+          </p>
+          <h3>A11Y - Example</h3>
           <p>
             <ul>
               <li><a href="https://usability.yale.edu/web-accessibility/accessibility-yale">Accessibility at Yale</a></li>
+              <li><a href="https://clever.com/">Clever</a></li>
+              <li><a href="http://web-accessibility.carnegiemuseums.org/">Carnegie Museums of Pittsburgh - Innovation Studio LogoWeb Accessibility Guidelines</a></li>
+              <li><a href="https://www.apple.com/accessibility/">Apple - Accessibility</a></li>
+              <li><a href="https://pattern-library.dequelabs.com/">Deque - Pattern library</a></li>
+              <li><a href="https://a11y-style-guide.com/style-guide/">A11Y - Style Guide</a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
             </ul>
           </p>
-          <h3>A11Y - Tools</h3>
+          <h3>A11Y - Tools/Testing</h3>
           <p>
             <ul>
               <li><a href="https://marcozehe.de/2018/04/11/introducing-the-accessibility-inspector-in-the-firefox-developer-tools/">Accessibility Inspector (Firefox)</a></li>
+              <li><a href="http://wave.webaim.org/">WAVE Web Accessibility Evaluation Tool</a></li>
+              <li><a href="https://marcozehe.de/2018/04/11/introducing-the-accessibility-inspector-in-the-firefox-developer-tools/">Accessibility Inspector (Firefox)</a></li>
+              <li><a href="https://littleforest.co.uk/">Little Forest</a></li>
+              <li><a href="http://squizlabs.github.io/HTML_CodeSniffer/">HTML CodeSniffer</a></li>
+              <li><a href="https://accessibilityinsights.io/">Accessibility Insights</a></li>
+              <li><a href="https://webhint.io/">webhint</a></li>
+              <li><a href="https://webaim.org/resources/contrastchecker/">WebAIM - Contrast Checker</a></li>
+              <li><a href="https://accessibe.com/">accessiBe</a></li>
+              <li><a href="https://www.html5accessibility.com/">HTML5 Accessibility</a></li>
+              <li><a href="https://ffoodd.github.io/a11y.css/">a11y.css - Extension</a></li>
+              <li><a href="https://css-tricks.com/accessibility-testing-tools/">Accessibility Testing Tools</a></li>
+              <li><a href="https://www.sitepoint.com/web-accessibility-tools-considerations/">Web Accessibility: Tools and Considerations</a></li>
+              <li><a href="https://daverupert.com/2018/07/assistive-technologies-i-test-with/">Assistive Technologies I Test With</a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
             </ul>
           </p>
           <h3>A11Y - Forms</h3>
           <p>
             <ul>
               <li><a href="https://marcozehe.de/2018/04/11/introducing-the-accessibility-inspector-in-the-firefox-developer-tools/">Accessibility Inspector (Firefox)</a></li>
-            </ul>
-          </p>
-          <!-- <quote-card /> -->
-          <h3>A11Y - Testing</h3>
-          <p>
-            <ul>
-              <li><a href="https://marcozehe.de/2018/04/11/introducing-the-accessibility-inspector-in-the-firefox-developer-tools/">Accessibility Inspector (Firefox)</a></li>
+              <li><a href="https://medium.com/@roblcopeland/how-can-i-make-my-forms-a-little-more-accessible-1726d63210f2">How Can I Make My Forms A Little More Accessible?</a></li>
+              <li><a href="https://designsystem.digital.gov/components/form-controls/">USWDS - Form controls</a></li>
+              <li><a href="https://designsystem.digital.gov/components/form-templates/">USWDS - Form templates</a></li>
+              <li><a href="https://adamsilver.io/articles/where-to-put-buttons-in-forms/">Where to put buttons on forms</a></li>
+              <li><a href="https://24ways.org/2019/making-a-better-custom-select-element/">Making a Better Custom Select Element</a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
             </ul>
           </p>
           <h3>A11Y - Legal</h3>
@@ -62,12 +144,45 @@
             <ul>
               <li><a href="https://www.d.umn.edu/~lcarlson/atteam/lawsuits.html">Higher Ed Accessibility Lawsuits, Complaints, and Settlements</a></li>
               <li><a href="https://www.insidehighered.com/news/2018/11/06/universities-still-struggle-make-websites-accessible-all">Feds Prod Universities to Address Website Accessibility Complaints</a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
             </ul>
           </p>
           <h3>A11Y - Influencers</h3>
           <p>
             <ul>
               <li><a href="https://www.kpodnar.com/">Kristina Podnar</a></li>
+              <li><a href="https://www.sarasoueidan.com/">Sara Soueidan</a></li>
+              <li><a href="https://www.sergeikriger.com/">Sergei Kriger</a></li>
+              <li><a href="https://www.aaron-gustafson.com/">Aaron Gustafson</a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+            </ul>
+          </p>
+          <h3>A11Y - Code</h3>
+          <p>
+            <ul>
+              <li><a href="https://codepen.io/shadeed/pen/PMygee">CodePen - Hamburger Menu</a></li>
+              <li><a href="https://css-tricks.com/having-a-little-fun-with-custom-focus-styles/">Having a Little Fun With Custom Focus Styles</a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+            </ul>
+          </p>
+          <h3>A11Y - Documents</h3>
+          <p>
+            <ul>
+              <li><a href="https://docs.google.com/document/d/1Bka7B6oeyF0N8H_kpR0oWtNQqoZkwvv1YcSlBXOy0CU/edit?usp=sharing">Kristina Podnar - Branding Policy</a></li>
+              <li><a href="https://docs.google.com/document/d/1cgjG_LYLcjxJkklF-JkOxo-mFNX9cKXjx0WVT9Stl7U/edit?usp=sharing">Kristina Podnar - Logo standard</a></li>
+              <li><a href="https://docs.google.com/document/d/1lLZWnkZPWn8HVjR3nkH1lv2AHscaHjntjbeKPrKtp-M/edit?usp=sharing">Kristina Podnar - Privacy policy</a></li>
+              <li><a href="https://docs.google.com/document/d/1ZeHNfqBDOXd7ZmTKIG9UFVukH75AJqZyAsEyM5QQ7dM/edit">Kristina Podnar - Privacy statement</a></li>
+              <li><a href="https://docs.google.com/document/d/12bQ7zFqiS5KLnDSC4nQU60JoL0CJarujsDUtlEB_KXI/edit?usp=sharing">Kristina Podnar - Accessibility policy we developed for Greg V</a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
+              <li><a href=""></a></li>
             </ul>
           </p>
         </div>
